099f35543aa0721f1cb3efb90a2e0e97c407493d
Bazu-Bazu <iv.dima1304@gmail.com>
2025-09-13 18:54:50 +0300

    Added entities basket and basketItem in service-order

--- service-order/src/main/java/com/marketplace/serviceOrder/entity/Basket.java
+++ service-order/src/main/java/com/marketplace/serviceOrder/entity/Basket.java
@@ -1,4 +1,33 @@
 package com.marketplace.serviceOrder.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "baskets")
+@Data
 public class Basket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @OneToMany(mappedBy = "basket", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BasketItem> items;
+
+    public void addItem(BasketItem item) {
+        items.add(item);
+        item.setBasket(this);
+    }
+
+    public void removeItem(BasketItem item) {
+        items.remove(item);
+        item.setBasket(null);
+    }
+
 }
--- service-order/src/main/java/com/marketplace/serviceOrder/entity/BasketItem.java
+++ service-order/src/main/java/com/marketplace/serviceOrder/entity/BasketItem.java
@@ -1,4 +1,34 @@
 package com.marketplace.serviceOrder.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "basket_items")
+@Data
 public class BasketItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "product_description")
+    private String productDescription;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(name = "seller_name", nullable = false)
+    private String sellerName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "basket_id", nullable = false)
+    private Basket basket;
+
 }
