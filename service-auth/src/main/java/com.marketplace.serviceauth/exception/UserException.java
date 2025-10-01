865ca5cf0bc6e946fe00cfac089091be783ce8c6
Bazu-Bazu <iv.dima1304@gmail.com>
2025-10-01 20:55:26 +0300

    Changed UserService.

---
+++ service-auth/src/main/java/com.marketplace.serviceauth/exception/UserException.java
@@ -0,0 +1,9 @@
+package com.marketplace.serviceauth.exception;
+
+public class UserException extends RuntimeException {
+
+    public UserException(String message) {
+        super(message);
+    }
+
+}
--- service-auth/src/main/java/com.marketplace.serviceauth/service/UserService.java
+++ service-auth/src/main/java/com.marketplace.serviceauth/service/UserService.java
@@ -5,7 +5,7 @@ import com.marketplace.serviceauth.dto.request.UpdateUserRequest;
 import com.marketplace.serviceauth.dto.response.UserResponse;
 import com.marketplace.serviceauth.entity.User;
 import com.marketplace.serviceauth.enums.Role;
-import com.marketplace.serviceauth.exception.UserAlreadyExistsException;
+import com.marketplace.serviceauth.exception.UserException;
 import com.marketplace.serviceauth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.function.Consumer;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +26,7 @@ public class UserService {
     @Transactional
     public void registerUser(RegisterUserRequest request) {
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new UserAlreadyExistsException("The user with this email already exists.");
+            throw new UserException("The user with this email already exists.");
         }
 
         User user = new User();
@@ -36,76 +37,63 @@ public class UserService {
         user.setBirthDate(request.getBirthDate());
         user.setCreatedAt(LocalDate.now());
         user.setRole(Role.ROLE_USER);
-
         userRepository.save(user);
+    }
+
+    @Transactional
+    public void enableTheUser(User user) {
+       user.setEnabled(true);
+       userRepository.save(user);
     }
 
     @Transactional
     public UserResponse updateUser(String email, UpdateUserRequest request) {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+        User user = findUserByEmail(email);
 
-        if (request.getFirstName() != null) {
-            user.setFirstName(request.getFirstName());
-        }
-
-        if (request.getLastName() != null) {
-            user.setLastName(request.getLastName());
-        }
-
-        if (request.getEmail() != null) {
-            user.setEmail(user.getEmail());
-        }
-
-        if (request.getPassword() != null) {
-            user.setPassword(user.getPassword());
-        }
-
-        if (request.getBirthDate() != null) {
-            user.setBirthDate(request.getBirthDate());
-        }
-
-        if (request.getPhone() != null) {
-            user.setPhone(request.getPhone());
-        }
-
+        updateIfNotNull(request.getFirstName(), user::setFirstName);
+        updateIfNotNull(request.getLastName(), user::setLastName);
+        updateIfNotNull(request.getEmail(), user::setEmail);
+        updateIfNotNull(request.getPassword(), pwd ->
+                user.setPassword(passwordEncoder.encode(pwd)));
+        updateIfNotNull(request.getBirthDate(), user::setBirthDate);
+        updateIfNotNull(request.getPhone(), user::setPhone);
         userRepository.save(user);
 
-        return UserResponse
-                .builder()
-                .message("Your data has been successfully updated.")
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
-                .email(user.getEmail())
-                .phone(user.getPhone())
-                .birthDate(user.getBirthDate())
-                .build();
+        return buildUserResponse(user);
+    }
+
+    private <T> void updateIfNotNull(T value, Consumer<T> setter) {
+        if (value != null) {
+            setter.accept(value);
+        }
     }
 
     public UserResponse getUser(String email) {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+        User user = findUserByEmail(email);
 
-        return UserResponse
-                .builder()
-                .message("Your personal information.")
+        return buildUserResponse(user);
+    }
+
+    @Transactional
+    public void deleteUser(String email) {
+        User user = findUserByEmail(email);
+        userRepository.delete(user);
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+    }
+
+    private UserResponse buildUserResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
                 .firstName(user.getFirstName())
                 .lastName(user.getLastName())
                 .email(user.getEmail())
                 .phone(user.getPhone())
+                .role(user.getRole())
                 .birthDate(user.getBirthDate())
-                .build();
-    }
-
-    @Transactional
-    public UserResponse deleteUser(String email) {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
-
-        userRepository.delete(user);
-
-        return UserResponse.builder()
-                .message("Your account has been successfully deleted.")
                 .build();
     }
 
