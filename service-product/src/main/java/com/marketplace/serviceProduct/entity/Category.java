5f62340a9accb7bc9e27be0eb9da50c178bca3b3
Bazu-Bazu <iv.dima1304@gmail.com>
2025-10-03 20:42:27 +0300

    Updated Category entity

--- service-product/src/main/java/com/marketplace/serviceProduct/entity/Category.java
+++ service-product/src/main/java/com/marketplace/serviceProduct/entity/Category.java
@@ -3,6 +3,7 @@ package com.marketplace.serviceProduct.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -21,7 +22,19 @@ public class Category {
     @JoinColumn(name = "parent_id")
     private Category parent;
 
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> children = new ArrayList<>();
+
     @ManyToMany(mappedBy = "categories")
     private List<Product> products;
+
+    public void addChild(Category child) {
+        children.add(child);
+        child.setParent(this);
+    }
+
+    public void removeChild(Category child) {
+        children.remove(child);
+    }
 
 }
