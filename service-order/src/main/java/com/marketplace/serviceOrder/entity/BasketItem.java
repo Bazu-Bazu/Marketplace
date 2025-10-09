2385581ce5e111b639c8524a8b5f58a64e854db5
Bazu-Bazu <iv.dima1304@gmail.com>
2025-10-09 12:59:55 +0300

    Changed BasketItem entity

--- service-order/src/main/java/com/marketplace/serviceOrder/entity/BasketItem.java
+++ service-order/src/main/java/com/marketplace/serviceOrder/entity/BasketItem.java
@@ -15,20 +15,8 @@ public class BasketItem {
     @Column(name = "product_id", nullable = false)
     private Long productId;
 
-    @Column(name = "product_name", nullable = false)
-    private String productName;
-
-    @Column(name = "product_description")
-    private String productDescription;
-
-    @Column(nullable = false)
-    private Integer price;
-
     @Column(nullable = false)
     private Integer count;
-
-    @Column(name = "seller_name", nullable = false)
-    private String sellerName;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "basket_id", nullable = false)
