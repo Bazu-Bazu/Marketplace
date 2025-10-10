4d9a18ac0d1905edf976010ca34064fbedf180bb
Bazu-Bazu <iv.dima1304@gmail.com>
2025-10-10 20:21:26 +0300

    Added the gRPC

--- service-product/src/test/java/marketplace/serviceproduct/ServiceProductApplicationTests.java
+++ service-product/src/test/java/marketplace/serviceproduct/ServiceProductApplicationTests.java
@@ -1,13 +1,13 @@
 package marketplace.serviceproduct;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceProductApplicationTests {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProductApplicationTests.class, args);
     }
 
 }
