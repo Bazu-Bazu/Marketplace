3c6eb8b0eb4146388873bd9b42ffbe1510662dd1
Bazu-Bazu <iv.dima1304@gmail.com>
2025-11-07 19:00:55 +0300

    Updated BasketService

--- service-order/src/main/java/com/marketplace/serviceOrder/service/BasketItemService.java
+++ service-order/src/main/java/com/marketplace/serviceOrder/service/BasketItemService.java
@@ -1,5 +1,7 @@
 package com.marketplace.serviceOrder.service;
 
+import com.marketplace.serviceOrder.dto.grpc.BasketItemValidationResult;
+import com.marketplace.serviceOrder.dto.grpc.BasketValidationResult;
 import com.marketplace.serviceOrder.dto.request.AddItemRequest;
 import com.marketplace.serviceOrder.dto.response.BasketItemResponse;
 import com.marketplace.serviceOrder.entity.Basket;
@@ -10,7 +12,7 @@ import com.marketplace.serviceOrder.service.grpc.ProductGrpcClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -29,20 +31,64 @@ public class BasketItemService {
         Basket basket = basketService.findBasketByUserId(userId);
 
         BasketItem basketItem = basketItemRepository.findByBasketAndProductId(basket, request.getProductId())
-                .orElseGet(() -> createBasketItem(basket, request.getProductId()));
+                .orElseGet(() -> createBasketItem(basket, request));
 
         int newCount = basketItem.getCount() + request.getCount();
         basketItem.setCount(newCount);
 
         basketItemRepository.save(basketItem);
 
+        basketService.updateBasketTotalPrice(basket);
+
         return buildBasketItemResponse(basketItem);
     }
 
-    private BasketItem createBasketItem(Basket basket, Long productId) {
+    public void updateBasketItemPrices(List<BasketItem> items, BasketValidationResult validationResult) {
+        List<BasketItem> updatedItems = items.stream().map(item -> {
+            Long productId = item.getProductId();
+
+            BasketItemValidationResult itemValidationResult = validationResult.getResultForItem(productId);
+
+            if (!item.getPrice().equals(itemValidationResult.getCurrentPrice())) {
+                item.setPrice(itemValidationResult.getCurrentPrice());
+            }
+
+            return item;
+        })
+        .toList();
+
+        basketItemRepository.saveAll(updatedItems);
+    }
+
+    public void deleteFromBasketNotExistItems(List<BasketItem> items, BasketValidationResult validationResult) {
+        for (BasketItem item : items) {
+            BasketItemValidationResult result = validationResult.getResultForItem(item.getProductId());
+
+            if (result == null) {
+                basketItemRepository.delete(item);
+                items.remove(item);
+            }
+        }
+    }
+
+    public void changeItemsCount(List<BasketItem> items, BasketValidationResult validationResult) {
+        items.forEach(item -> {
+            BasketItemValidationResult result = validationResult.getResultForItem(item.getProductId());
+            if (!result.isCountSufficient()) {
+                item.setCount(result.getAvailableCount());
+                basketItemRepository.save(item);
+            }
+        });
+    }
+
+    private BasketItem createBasketItem(Basket basket, AddItemRequest request) {
         BasketItem item = new BasketItem();
-        item.setProductId(productId);
+        item.setProductId(request.getProductId());
+        item.setCount(request.getCount());
+        item.setPrice(request.getPrice());
         item.setBasket(basket);
+
+        basket.getItems().add(item);
 
         return item;
     }
--- service-order/src/main/java/com/marketplace/serviceOrder/service/BasketService.java
+++ service-order/src/main/java/com/marketplace/serviceOrder/service/BasketService.java
@@ -1,17 +1,31 @@
 package com.marketplace.serviceOrder.service;
 
+import com.marketplace.serviceOrder.dto.grpc.BasketValidationResult;
 import com.marketplace.serviceOrder.entity.Basket;
+import com.marketplace.serviceOrder.entity.BasketItem;
 import com.marketplace.serviceOrder.exception.BasketException;
 import com.marketplace.serviceOrder.repository.BasketRepository;
-import lombok.RequiredArgsConstructor;
+import com.marketplace.serviceOrder.service.grpc.ProductGrpcClient;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
-@RequiredArgsConstructor
 public class BasketService {
 
+    private final BasketItemService basketItemService;
     private final BasketRepository basketRepository;
+    private final ProductGrpcClient productGrpcClient;
+
+    public BasketService(@Lazy BasketItemService basketItemService, BasketRepository basketRepository,
+                         ProductGrpcClient productGrpcClient) {
+        this.basketItemService = basketItemService;
+        this.basketRepository = basketRepository;
+        this.productGrpcClient = productGrpcClient;
+    }
 
     @Transactional
     public void createBasket(Long userId) {
@@ -24,6 +38,44 @@ public class BasketService {
     public Basket findBasketByUserId(Long userId) {
         return basketRepository.findByUserId(userId)
                 .orElseThrow(() -> new BasketException("Basket not found."));
+    }
+
+    public void updateBasketTotalPrice(Basket basket) {
+        List<BasketItem> items = basket.getItems();
+
+        int totalPrice = items.stream()
+                .mapToInt(item -> {
+                    int price = item.getPrice() != null ? item.getPrice() : 0;
+                    int count = item.getCount() != null ? item.getCount() : 0;
+                    return price * count;
+                })
+                .sum();
+
+        basket.setTotalPrice(totalPrice);
+        basketRepository.save(basket);
+    }
+
+    public BasketValidationResult validateBasket(Basket basket) {
+        List<BasketItem> items = basket.getItems();
+
+        if (items.isEmpty()) {
+            return new BasketValidationResult(Map.of());
+        }
+
+        BasketValidationResult validationResult = productGrpcClient.validateBasketItems(items);
+
+        if (validationResult.hasNotExistItems()) {
+            basketItemService.deleteFromBasketNotExistItems(items, validationResult);
+        }
+
+        if (validationResult.hasNotCountSufficientItems()) {
+            basketItemService.changeItemsCount(items, validationResult);
+        }
+
+        basketItemService.updateBasketItemPrices(items, validationResult);
+        updateBasketTotalPrice(basket);
+
+        return validationResult;
     }
 
 }
