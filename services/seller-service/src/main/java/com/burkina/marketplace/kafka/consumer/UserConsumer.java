9c60610678180229b18be0da493da36052e3073c
BazuBazu <iv.dima1304@gmail.com>
2026-08-21 23:23:41 +0300

    Added UserConsumer

---
+++ services/seller-service/src/main/java/com/burkina/marketplace/kafka/consumer/UserConsumer.java
@@ -0,0 +1,26 @@
+package com.burkina.marketplace.kafka.consumer;
+
+import com.burkina.common.dto.event.UserDeletedEvent;
+import com.burkina.marketplace.service.SellerCommandService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserConsumer {
+
+    private final ObjectMapper objectMapper;
+    private final SellerCommandService sellerCommandService;
+
+    @KafkaListener(topics = "user-deleted")
+    public void handleUserDeletedEvent(String message, Acknowledgment ack) throws JsonProcessingException {
+        UserDeletedEvent event = objectMapper.readValue(message, UserDeletedEvent.class);
+        sellerCommandService.delete(event.userId());
+
+        ack.acknowledge();
+    }
+}
