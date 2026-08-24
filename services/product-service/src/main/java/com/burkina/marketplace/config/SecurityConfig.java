aae7295da2d862ad578a4ecf40aadc96f0f5ecac
BazuBazu <iv.dima1304@gmail.com>
2026-08-25 00:46:47 +0300

    Added the security configuration and aop for logging

--- services/product-service/src/main/java/com/burkina/marketplace/config/SecurityConfig.java
+++ services/product-service/src/main/java/com/burkina/marketplace/config/SecurityConfig.java
@@ -1,45 +1,39 @@
-package com.marketplace.serviceProduct.config;
+package com.burkina.marketplace.config;
 
-import com.marketplace.serviceProduct.service.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtFilter jwtFilter;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(
-                                HttpMethod.GET, "/product/**",
-                                "product-photo/**")
-                        .permitAll()
-                        .requestMatchers(
-                                "/product/add",
-                                "/product-photo/**")
-                        .hasRole("SELLER")
-                        .requestMatchers(
-                                "/category/**")
-                        .hasRole("ADMIN"))
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
+                        .sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
+                )
+                .build();
     }
-
 }
